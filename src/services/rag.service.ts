29dddf5e316cfe0774b653f8a879885933c21dc7
Infan-Jioun <infanjiounrahman20606@gmail.com>
2026-06-09 12:44:15 +0600

    feat: create some interface for reg

--- src/services/rag.service.ts
+++ src/services/rag.service.ts
@@ -5,6 +5,27 @@ export interface IRagQueryPayload {
     limit?: string;
     sourceType?: string
 }
+export interface IRagSource {
+    id: string;
+    content: string;
+    similarity: number;
+    metadata?: {
+        name?: string;
+        [key: string]: unknown;
+    }
+    sourceType?: string;
+
+}
+export interface IRagQueryData {
+    answer: any;
+    souces: IRagSource[];
+    contextUsed: string
+}
+export interface IIngestDoctorData {
+    success: boolean;
+    message: string;
+    indexedCount: number
+}
 export const queryRagService = async (payload: IRagQueryPayload) => {
     const response = await httpClient.post("/rag/query", payload);
     return response;
